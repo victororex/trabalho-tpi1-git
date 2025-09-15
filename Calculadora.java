37df2e858cf45e1e0e30b7ececad59ceced83c4a
Victor Alexandre <hyperodroquin10@gmail.com>
2025-09-15 18:57:13 -0300

    Update Calculadora.java
    feat(aluno1,aluno4/Cecília, Victor):Marge oresgate commit
    
    Co-Authored-By: ceciliabarbosaf <232245992+ceciliabarbosaf@users.noreply.github.com>

--- Calculadora.java
+++ Calculadora.java
@@ -1,29 +1,25 @@
-public class Calculadora {
+    public class Calculadora {
 
     /**
      * Executa uma operação matemática básica.
-     * @param operacao
-     * @param operacao Tipo da operação: "soma"
+     * @param operacao Tipo da operação: "subtracao", "multiplicacao"
      * @param a Primeiro número
      * @param b Segundo número
      * @return Resultado da operação
      */
     public double calcular(String operacao, int a, int b) {
         switch (operacao.toLowerCase()) {
-            default:
-                throw new IllegalArgumentException("Operação inválida: " + operacao);
-            case "soma":
-                return a + b;
-            
+            case "subtracao":
+                return a - b;
+            case "multiplicacao":
+                return a * b;
         }
     }
 
     // Método principal para testar
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
-
-        calc.calcular( "", 0, 0 );
-        System.out.println("Soma: " + calc.calcular("soma", 10, 5));
+        System.out.println("Subtração: " + calc.calcular("subtracao", 10, 5));
+        System.out.println("Multiplicação: " + calc.calcular("multiplicacao", 10, 5));
     }
 }
-
