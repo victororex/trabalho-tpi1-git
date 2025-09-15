3ead073c19ed8fd6b13015d94f59cd975a2ef817
caual1m4 <caualimap.10@gmail.com>
2025-09-15 18:58:15 -0300

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -3,8 +3,6 @@ public class Calculadora {
     /**
      * Executa uma operação matemática básica.
      * @param operacao
-     * @param operacao Tipo da operação: "soma"
-     * @param operacao Tipo da operação: "divisao"
      * @param a Primeiro número
      * @param b Segundo número
      * @return Resultado da operação
@@ -13,14 +11,6 @@ public class Calculadora {
         switch (operacao.toLowerCase()) {
             default:
                 throw new IllegalArgumentException("Operação inválida: " + operacao);
-            case "soma":
-                return a + b;
-            
-            case "divisao":
-                if (b == 0) {
-                    throw new IllegalArgumentException("Divisão por zero não é permitida!");
-                }
-                return (double) a / b;
         }
     }
 
@@ -29,8 +19,6 @@ public class Calculadora {
         Calculadora calc = new Calculadora();
 
         calc.calcular( "", 0, 0 );
-        System.out.println("Soma: " + calc.calcular("soma", 10, 5));
-        System.out.println("Divisão: " + calc.calcular("divisao", 10, 5));
     }
 }
 
